634b79a1494ce1d72af61056a568129f99fcb58c
Alys <alice.g.cooper@gmail.com>
2022-06-10 14:50:15 -0400

    Create div container and gameboard

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//will have to create new repo and push files to your personal github page
+
+// spend 30 mins-1 hr planning out how this will be approached
+
+// create game board
+
+// doesn't have to be X or O - just two sets of images
+// robot versus human
+
+//easier way to test draw conditions - start at the middle going down: down middle, up left, down right
+
+//this is mainly to test JS functionality, but have fun with CSS too!
