8bd95cc031c959f6e029e17bdd38b0ecb998348c
Alys <alice.g.cooper@gmail.com>
2022-06-10 15:48:00 -0400

    Add Reset Button and Styling, Outline Psuedocode in app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,43 @@
-//will have to create new repo and push files to your personal github page
-
 // spend 30 mins-1 hr planning out how this will be approached
-
 // create game board
+// easier way to test draw conditions - start at the middle going down: down middle, up left, down right
 
-// doesn't have to be X or O - just two sets of images
-// robot versus human
+// reset button on click should reset inner html for container
 
-//easier way to test draw conditions - start at the middle going down: down middle, up left, down right
 
-//this is mainly to test JS functionality, but have fun with CSS too!
+// on square click, we want to
+//  1. set background of div to either crow (image1) or forrester (image2)
+//  2. change the "turn order" variable to either Player 1 or Player 2
+//      2a. This should also change the "player1/player2 turn" text
+//  3. change over the background image to either image1 or image2
+//  4. WE WILL NEED A "MOVETRACKER" VARIABLE
+//  5. We will need to disable further clicks
+
+// we want to evaluate a win condition after each turn
+// what does a win condition look like? 8 win conditions
+// the same image in the following combinations
+//  1,2,3
+//  4,5,6
+//  7,8,9
+//  7,4,1
+//  8,5,2
+//  9,6,3
+//  7,5,3
+//  9,5,1
+
+// on WIN condition, what do we want it to do?
+// 1. update the message that says "PLAYER ONE WIN!/PLAYER TWO WIN!"
+// 2. Update Player 1 / Player 2 Scoreboard
+// 3. If player1 or player2 score = 3, declare winner!
+//  3a. Update final 'div' text with final score
+// 4. Disable further clicks
+
+// BONUS:
+// track players wins over time
+// add simple AI that has the computer pick an random empty square
+// Make your computer seem more human by adding a short time delay between your turn and the computer's turn.
+
+// SUPER DUPER BONUS
+// Add an AI that can beat you every time with the mini-max algorithm.
+
+
